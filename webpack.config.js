8b75611d7f9db4e1d2367b5c43c8a8aad891f503
fujioka8700 <fujioka8700@gmail.com>
2021-07-10 22:19:58 +0900

    ハッシュについて

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,50 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
     devtool: 'eval',
     // entry: './src/app.js',
-    entry: { app:'./src/app.js' },
+    // entry: { app:'./src/app.js' },
     // entry: ['./src/app.js', './src/sub.js'], // 依存していない状態
-    // entry: {
-    //     app: './src/app.js', // 分割して出力
-    //     sub: './src/sub.js'
-    // },
+    entry: {
+        app: './src/app.js', // エントリーポイント追加
+        sub: './src/sub.js'
+    },
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: '[name].bundle.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'postcss-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg|woff2?|ttf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[contenthash].[ext]',
+                            outputPath: 'images',
+                            publicPath: 'images'
+                        }
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[chunkhash].css'
+        }),
+    ]
 }
